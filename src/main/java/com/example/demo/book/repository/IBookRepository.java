0611170149e9c61b7daa793edbfd49201f98fb18
Sébastien Benede <sebastien.benede@ratp.fr>
2022-10-24 14:57:27 +0200

    Implement book controller

---
+++ src/main/java/com/example/demo/book/repository/IBookRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.book.repository;
+
+import com.example.demo.book.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IBookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitleOrAuthor(@Param("name") String name);
+}
